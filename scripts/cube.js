895e177bc3af6c1fc0247295daf7e3521a6e05bf
Heather Mahan <heatheramahan@gmail.com>
2015-11-25 11:27:05 -0800

    initial commit

---
+++ scripts/cube.js
@@ -0,0 +1,43 @@
+// Tutorial from:
+// http://threejs.org/docs/index.html#Manual/Introduction/Creating_a_scene
+
+// Creating the scene.
+// To actually be able to display anything with Three.js, we need three things:
+// A scene, a camera, and a renderer so we can render the scene with the camera.
+var scene = new THREE.Scene(),
+    // camera args: FOV, aspect ratio, near and far clipping plane
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ),
+    renderer = new THREE.WebGLRenderer();
+// Set the size at which we want it to render our app.
+renderer.setSize( window.innerWidth, window.innerHeight );
+// Add the renderer element to our HTML document.
+// This is a <canvas> element the renderer uses to display the scene to us.
+document.body.appendChild( renderer.domElement );
+
+// Adding a cube.
+// To create a cube, we need three things: a BoxGeometry, a material, and a mesh.
+// BoxGeometry is an object that contains all the points (vertices) and fill (faces) of the cube.
+// All materials take an object of properties which will be applied to them.
+// A mesh is an object that takes a geometry, and applies a material to it,
+// which we then can insert to our scene, and move freely around.
+var geometry = new THREE.BoxGeometry( 1, 1, 1 ),
+    material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ),
+    cube = new THREE.Mesh( geometry, material );
+// By default, when we call scene.add(), the thing we add will be added to the
+// coordinates (0,0,0). This would cause both the camera and the cube to be
+// inside each other. To avoid this, we simply move the camera out a bit.
+scene.add( cube );
+camera.position.z = 5;
+
+// Rendering the scene.
+// This will create a loop that causes the renderer to draw the scene 60 times
+// per second. Basically, anything you want to move or change while the app is
+// running has to go through the render loop.
+function render() {
+    requestAnimationFrame( render );
+    // Rotate the cube slightly each frame.
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+render();
